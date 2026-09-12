156b88d5ae1626516253a2a431b3cc496ab1a96b
valerochka1337 <valerochkayartsev88@gmail.com>
2026-09-13 01:33:34 +0300

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,8 +63,8 @@ android {
     applicationId = "com.valerochka1337.valerochkagym"
     minSdk = 36
     targetSdk = 37
-    versionCode = testVersionCode ?: 65
-    versionName = testVersionName ?: "1.3.57"
+    versionCode = testVersionCode ?: 66
+    versionName = testVersionName ?: "1.3.58"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
